e62fffb53e8fc6430fad36312d69d6275fa42f02
zhilu12 <zhilinlu12@gmail.com>
2024-10-07 22:04:17 -0400

    submission for HW3

--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
@@ -1,5 +1,23 @@
 from typing import Optional
 
-class MigrationPath:
+from wildlife_tracker.habitat_management.habitat import Habitat
 
-    pass
+class MigrationPath:
+    def __init__(self, 
+                 path_id: int, 
+                 species: str, 
+                 start_location: Habitat,
+                 destination: Habitat,
+                 duration: Optional[int] = None) -> None:
+        self.path_id = path_id
+        self.species = species  
+        self.start_location = start_location
+        self.destination = destination
+        self.duration = duration
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
+
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
+
